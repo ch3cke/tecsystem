6d0fc821abea331aaef4f5d6bb298d95172a855e
ch3cke <ch3cke@gmail.com>
2020-01-01 01:26:05 +0800

    tesr

---
+++ src/tools/untils/encrypt.java
@@ -0,0 +1,48 @@
+package tools.Db_tools;
+
+import java.security.MessageDigest;
+
+public class encrypt {
+    /**全局数组**/
+    private final static String[] strDigits = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F" };
+
+    public static String encryptToMD5(String str){
+        String hexStr="";
+        try{
+            MessageDigest md5 = MessageDigest.getInstance("MD5");
+            hexStr = byteToString(md5.digest(str.getBytes()));
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return hexStr;
+    }
+
+    /**
+     * 转换字节数组为16进制字串
+     * @param bByte
+     * @return
+     */
+    private static String byteToString(byte[] bByte) {
+        StringBuffer sBuffer = new StringBuffer();
+        for (int i = 0; i < bByte.length; i++) {
+            sBuffer.append(byteToArrayString(bByte[i]));
+        }
+        return sBuffer.toString();
+    }
+
+    /**
+     * 返回形式为数字跟字符串
+     * @param bByte
+     * @return
+     */
+    private static String byteToArrayString(byte bByte) {
+        int iRet = bByte;
+        if (iRet < 0) {
+            iRet += 256;
+        }
+        int iD1 = iRet / 16;
+        int iD2 = iRet % 16;
+        return strDigits[iD1] + strDigits[iD2];
+    }
+
+}
